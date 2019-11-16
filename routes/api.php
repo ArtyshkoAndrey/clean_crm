6dd6c8116d124af94c4311068e9bdd702a868657
Artyshko Andrey <artyshko.andrey@gmail.com>
2019-11-16 23:42:25 +0700

    Тест для фильтрации проблем

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Model\Task;
+use App\Model\User;
+use Carbon\Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,69 @@ use App\Model\Task;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('/test', function (){
+  $filters = array(
+    ['name' => "Статус", 'field' => "status", 'value' => "complete", 'type' => "select"]
+  );
+  $user = User::where('id', 1)->first();
+  $tasks = $user->tasks;
+    foreach($filters as $filter) {
+      switch($filter['type']) {
+        case 'text': {
+          $tasks = $tasks->where($filter['field'], $filter['value']);
+          break;
+        }
+        case 'date': {
+          $tasks = $tasks->where($filter['field'], $filter['value']);
+          break;
+        }
+        case 'select': {
+          switch($filter['field']) {
+            case 'identified': {
+              $tasks = $tasks->filter(function($item) use (&$filter) {
+                return $item->identified->contains('id', $filter['value']);
+              });
+              break;
+              // return $tasks;
+            }
+            case 'responsible': {
+              $tasks = $tasks->filter(function($item) use (&$filter) {
+                return $item->responsible->id == $filter['value'];
+              });
+              break;
+              // return $tasks;
+            }
+            case 'status': {
+              switch($filter['value']) {
+                case 'work': {
+                  $tasks = $tasks->where('target_date', '>=', Carbon::now()->toDateString())
+                    ->where('correction_date', null);
+                  break;
+                  // return $tasks;
+                }
+                case 'complete': {
+                  // return response()->json([$tasks, Carbon::now()->toDateString()], 200);
+                  $tasks = $tasks->where('correction_date', '!=', null)->where('correction_date', '<=', Carbon::now()->toDateTimeString());
+                  break;  
+                  // return $tasks;
+                }
+                case 'overdue': {
+                  $tasks = $tasks->where('target_date', '<', Carbon::now()->toDateString())
+                    ->where('correction_date', null);
+                  break;
+                  // return $tasks;
+                }
+              }
+              break;
+            }
+          }
+          break;
+        }
+      }
+    }
+    return [$tasks, Carbon::now()->toDateTimeString()];
+});
 
 Route::group(['prefix' => 'auth'], function () {
     Route::post('login','AuthController@authenticate');
